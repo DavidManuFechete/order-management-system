5d7b798c6fb55f4066f0489e1505a529c4120751
DavidManuFechete <fechetedavid10@gmail.com>
2023-10-27 00:43:06 +0300

    Commit

--- src/main/java/ro/tuc/connection/ConnectionFactory.java
+++ src/main/java/ro/tuc/connection/ConnectionFactory.java
@@ -17,7 +17,7 @@ public class ConnectionFactory {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DBURL = "jdbc:mysql://localhost:3306/orders_management";
     private static final String USER = "root";
-    private static final String PASS = "";
+    private static final String PASS = "davidxd117";
 
     private static ConnectionFactory singleInstance = new ConnectionFactory();
 
